97526625146ecc45cf2c20e3d6a2f65b35516fb5
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2021-11-16 10:32:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,15 +10,25 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static('public'));
-  
+
+// Get request for homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.js'));
 });
 
+// Get request for homepage
 app.get('/notes',(req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// Get request to read notes from file
+app.get('/notes', (req, res) => res.json((notes));
+
+// POST request to send notes to server        
+app.post('/notes', (req, res) => {
+  res.json(`${req.method} request received!`);
+  console.info(`${req.method} request received!`);
+});
 
         
 app.listen(PORT, () => 
