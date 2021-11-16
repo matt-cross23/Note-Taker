da661f6510e916c92ab697456099d1d9ea7ec547
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2021-11-16 14:15:39 -0500

    edit Get and Post

--- server.js
+++ server.js
@@ -16,18 +16,35 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.js'));
 });
 
-// Get request for homepage
+// Get request for notes
 app.get('/notes',(req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // Get request to read notes from file
-app.get('/notes', (req, res) => res.json((notes));
+app.get('/notes', (req, res) => {
+  res.status(200).json((notes);
+ });
 
 // POST request to send notes to server        
 app.post('/notes', (req, res) => {
   res.json(`${req.method} request received!`);
   console.info(`${req.method} request received!`);
+  const [{"title", "text"}] = req.body;
+  if (title && text) { 
+  const newNote ={
+    title, 
+    text,
+  };
+    const response = {
+      status: 'success'
+      body: newNote,
+    };
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+   res.status(500).json('Error in posting review');
+  }
 });
 
         
