9dcd5b6440b4b3ecc75f8b921022b2f9394693b5
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2021-11-16 14:41:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
 const notes = require('./db/db.json')
-
+const api = require(./routes/routes.js)
 const app = express();
 
-const PORT = 3001;
-
+const PORT = process.env.port || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
@@ -21,30 +20,8 @@ app.get('/notes',(req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// Get request to read notes from file
-app.get('/notes', (req, res) => {
-  res.status(200).json((notes);
- });
-
-// POST request to send notes to server        
-app.post('/notes', (req, res) => {
-  res.json(`${req.method} request received!`);
-  console.info(`${req.method} request received!`);
-  const [{"title", "text"}] = req.body;
-  if (title && text) { 
-  const newNote ={
-    title, 
-    text,
-  };
-    const response = {
-      status: 'success'
-      body: newNote,
-    };
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-   res.status(500).json('Error in posting review');
-  }
+app.get('*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '/public/404.html'))
 });
 
         
