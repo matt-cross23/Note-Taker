29faa7a078ad73990db9e851a8b21db5f4a199f7
Matthew Cross <matthewcross1017@gmail.com>
2021-11-17 21:51:44 -0500

    change PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require('./routes/indexroute.js')
 const app = express();
 
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
